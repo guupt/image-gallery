adafbb92007ef09538a23cbb6ce670dcdd7c2f59
guupt <gustavo.uptmoor@gmail.com>
2023-12-13 18:02:16 -0300

    finished up, bug in the prevPage calculus to fix

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,30 +1,48 @@
 import fetchImages from "@/app/lib/fetchImages";
 import type { Photo, ImagesResults } from "@/app/models/Images";
-import Image from "next/image";
 import ImgContainer from "./ImgContainer";
 import addBlurredDataUrl from "../lib/getBase64";
+import getPrevNextPage from "../lib/getPrevNextPage";
+import Footer from "./Footer";
 
 type Props = {
   topic?: string | undefined;
-  photo?: Photo;
+  page?: string | undefined;
 };
 
-export default async function Gallery({ topic }: Props) {
-  const url = !topic
-    ? `https://api.pexels.com/v1/curated`
-    : `https://api.pexels.com/v1/search?query=${topic}`;
-
+export default async function Gallery({ topic = "curated", page }: Props) {
+  let url;
+  if (topic === "curated") {
+    //browsing beyond home
+    url = `https://api.pexels.com/v1/curated?page=${page}`;
+  } else if (topic === "curated") {
+    //home
+    url = `https://api.pexels.com/v1/curated`;
+  } else if (!page) {
+    //1st page of search results
+    url = `https://api.pexels.com/v1/search?query=${topic}`;
+  } else {
+    //search results beyond 1st page
+    url = `https://api.pexels.com/v1/search?query=${topic}&page=${page}`;
+  }
   const images: ImagesResults | undefined = await fetchImages(url);
-  if (!images)
+  if (!images || images.per_page === 0)
     return <h2 className="m-4 text-2xl font-bold">No Images found</h2>;
 
   const photoWithBlur = await addBlurredDataUrl(images);
 
+  const { prevPage, nextPage } = getPrevNextPage(images);
+
+  const footerProps = { topic, page, nextPage, prevPage };
+
   return (
-    <section className="px-2 my-3 grid gap-2 grid-cols-gallery auto-rows[10px]">
-      {photoWithBlur.map((photo) => (
-        <ImgContainer key={photo.id} photo={photo} />
-      ))}
-    </section>
+    <>
+      <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
+        {photoWithBlur.map((photo) => (
+          <ImgContainer key={photo.id} photo={photo} />
+        ))}
+      </section>
+      <Footer {...footerProps} />
+    </>
   );
 }
